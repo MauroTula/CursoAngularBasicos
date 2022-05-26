667575bbdf91114dc0bd18fa7eb0515f748a98dc
Mauro Tula <maurotula@outlook.com>
2022-05-26 07:40:38 -0300

    wip

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,21 +1,30 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
 export class ListadoComponent {
-
-  heroes: string[] = ['Spiderman', 'Iron Man', 'Hulk', 'Thor', 'Capitán América'];  
-  heroeListaBorrado: string[] = [ ];
+  heroes: string[] = [
+    'Spiderman',
+    'Iron Man',
+    'Hulk',
+    'Thor',
+    'Capitán América',
+  ];
+  heroeListaBorrado: string[] = [];
+  disabled = false;
   /* heroeListaBorrado: string = ''; */
   /* O se puede hacer de esta forma pero solo aparecen de a uno a la vez */
-  
-  borrarHeroe(){
+
+  borrarHeroe() {
     const heroeBorrado = this.heroes.shift();
     this.heroeListaBorrado.push(heroeBorrado);
     /* this.heroeListaBorrado.push() || ''; */
+    console.log(this.heroes);
+    if (this.heroes.length === 0) {
+      this.disabled = true;
+    }
   }
-
 }
